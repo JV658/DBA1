667331f3c7874df5f4175a243f378dd282de52dd
JV658 <90655082+JV658@users.noreply.github.com>
2025-01-29 17:04:34 -0500

    Create A1.sql
    added questions 1-5

---
+++ A1.sql
@@ -0,0 +1,17 @@
+select vendor_id, SUM(invoice_total)
+from invoices
+GROUP BY vendor_id
+ORDER BY vendor_id;
+
+/*
+116.54
+1083.58
+*/
+
+select CAST(invoice_number AS SIGNED) from invoices order by invoice_number;
+
+
+
+use ex;
+
+select * from projects;
